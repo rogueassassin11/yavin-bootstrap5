457542816109d53e553b6e2db0ec20a266e6f643
rogueassassin11 <maecagadas2011@gmail.com>
2023-12-20 22:25:14 +0800

    Finished Yavin

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 // Navbar Sticky Scroll
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
     } else {
       navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
     }
   });
+}
+
+// Scroll to Top
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // Stat Increments
@@ -36,5 +45,7 @@ function incrementStats() {
   });
 }
 
+// EVENT LISTENERS
 document.addEventListener("DOMContentLoaded", userScroll);
 document.addEventListener("DOMContentLoaded", incrementStats);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
